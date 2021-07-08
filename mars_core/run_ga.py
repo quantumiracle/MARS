b571239c446b63d1673513b53121042e349dd278
zihan <zhding@mail.ustc.edu.cn>
2021-07-08 17:22:53 +0800

    fix run_ga

--- mars_core/run_ga.py
+++ mars_core/run_ga.py
@@ -1,12 +1,12 @@
 from utils.func import LoadYAML2Dict
 from env.import_env import make_env
-from rollout_ga import rollout_ga
+from rollout import rollout
 from rl.algorithm import *
 
 ### Load configurations
 # yaml_file = 'confs/gym_cartpolev1_ga'
-# yaml_file = 'confs/pettingzoo_boxingv1_selfplay_ga'
-yaml_file = 'confs/slimevolley_slimevolleyv0_selfplay_ga'
+yaml_file = 'confs/pettingzoo_boxingv1_selfplay_ga'
+# yaml_file = 'confs/slimevolley_slimevolleyv0_selfplay_ga'
 
 args = LoadYAML2Dict(yaml_file, toAttr=True, mergeDefault=True)
 
