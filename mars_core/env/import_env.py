fed8080433ed1e30d53cc805f85110455d3d69db
zihan <zhding@mail.ustc.edu.cn>
2021-07-26 18:20:48 +0800

    add ed exploit test

--- mars_core/env/import_env.py
+++ mars_core/env/import_env.py
@@ -43,7 +43,7 @@ pettingzoo_envs = {
         'basketball_pong_v1', 'boxing_v1', 'combat_plane_v1', 'combat_tank_v1',
         'double_dunk_v2', 'entombed_competitive_v2', 'entombed_cooperative_v2',
         'flag_capture_v1', 'foozpong_v1', 'ice_hockey_v1', 'joust_v2',
-        'mario_bros_v2', 'maze_craze_v2', 'othello_v2', 'pong_v1', 
+        'mario_bros_v2', 'maze_craze_v2', 'othello_v2', 'pong_v1', 'pong_v2', 
         'quadrapong_v2', 'space_invaders_v1', 'space_war_v1', 'surround_v1',
         'tennis_v2', 'video_checkers_v3', 'volleyball_pong_v1', 'warlords_v2',
         'wizard_of_wor_v2'
