e037fdd4572a638233798fe3a637292a8999e072
zihan <zhding@mail.ustc.edu.cn>
2021-07-27 17:21:06 +0800

    fix logger; test atari

---
+++ mars_core/test_atari.sh
@@ -0,0 +1,12 @@
+echo "Running DATE:" $(date +"%Y-%m-%d %H:%M")
+
+DATE=`date '+%Y%m%d_%H%M'`
+echo "Save as: " $DATE
+min=0
+max=1
+inter=1
+for ((i=min; i <= max; i+=inter));
+do 
+    echo python test_atari.py --id="$i"
+    nohup python test_atari.py --id="$i" > $DATE$i.log &
+done
