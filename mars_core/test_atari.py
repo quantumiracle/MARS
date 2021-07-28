1d2815071b899ae719db87e8f434d03cfbf2e643
zihan <zhding@mail.ustc.edu.cn>
2021-07-28 10:31:31 +0800

    update pettingzoo from 1.8 to 1.10

--- mars_core/test_atari.py
+++ mars_core/test_atari.py
@@ -12,6 +12,7 @@ parser_args = parser.parse_args()
 yaml_file = 'confs/pettingzoo_atari_nash_dqn_exploiter'
 args = LoadYAML2Dict(yaml_file, toAttr=True, mergeDefault=True)
 envs = ['basketball_pong_v2', 'surround_v1', 'boxing_v1']
+# ['double_dunk_v2', 'combat_plane_v1']
 args.env_name = envs[int(parser_args.id)]
 
 ### Create env
--- mars_core/test_env.py
+++ mars_core/test_env.py
@@ -16,14 +16,14 @@ envs = {
         'SlimeVolleyNoFrameskip-v0', 'SlimeVolleyPixel-v0'
     ],
     'pettingzoo': [
-        'basketball_pong_v1', 'boxing_v1', 'combat_plane_v1', 'combat_tank_v1',
+        'basketball_pong_v2', 'boxing_v1', 'combat_plane_v1', 'combat_tank_v1',
         'double_dunk_v2', 'entombed_competitive_v2', 'entombed_cooperative_v2',
-        'flag_capture_v1', 'foozpong_v1', 'ice_hockey_v1', 'joust_v2',
-        'mario_bros_v2', 'maze_craze_v2', 'othello_v2', 'pong_v1',
-        'quadrapong_v2', 'space_invaders_v1', 'space_war_v1', 'surround_v1',
-        'tennis_v2', 'video_checkers_v3', 'volleyball_pong_v1', 'warlords_v2',
-        'wizard_of_wor_v2', 'dou_dizhu_v3', 'go_v3', 'leduc_holdem_v3',
-        'rps_v1', 'texas_holdem_no_limit_v3', 'texas_holdem_v3',
+        'flag_capture_v1', 'foozpong_v2', 'ice_hockey_v1', 'joust_v2',
+        'mario_bros_v2', 'maze_craze_v2', 'othello_v2', 'pong_v2',
+        'quadrapong_v3', 'space_invaders_v1', 'space_war_v1', 'surround_v1',
+        'tennis_v2', 'video_checkers_v3', 'volleyball_pong_v2', 'warlords_v2',
+        'wizard_of_wor_v2', 'dou_dizhu_v3', 'go_v4', 'leduc_holdem_v3',
+        'rps_v2', 'texas_holdem_no_limit_v3', 'texas_holdem_v3',
         'tictactoe_v3', 'uno_v3'
     ],
     'lasertag':
@@ -38,10 +38,10 @@ if __name__ == "__main__":
     for env_type, envs in envs.items():
         for env_name in envs:
             cnt += 1
-            EnvArgs['name'] = env_name
-            EnvArgs['type'] = env_type
+            EnvArgs['env_name'] = env_name
+            EnvArgs['env_type'] = env_type
             env_args = AttrDict(EnvArgs)
-
+            test_env = make_env(env_args)
             try:
                 test_env = make_env(env_args)
             except:
