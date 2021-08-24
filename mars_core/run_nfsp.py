3b6b47934642eddb09c408808b96bcf8a5cc1264
quantumiracle <1402434478@qq.com>
2021-08-24 11:30:06 -0400

    add more resutls

--- mars_core/run_nfsp.py
+++ mars_core/run_nfsp.py
@@ -3,13 +3,27 @@ from env.import_env import make_env
 from rollout import rollout
 from rl.algorithm import *
 
+import argparse
+parser = argparse.ArgumentParser(
+    description='NFSP training.')
+parser.add_argument('--test', dest='test', action='store_true', default=False)
+parser.add_argument('--model', type=str, default=None,
+                    help='model index')
+parser_args = parser.parse_args()
+
+
 ### Load configurations
-# yaml_file = 'confs/pettingzoo_boxingv1_nfsp'
+yaml_file = 'confs/pettingzoo_boxingv1_nfsp'
 # yaml_file = 'confs/slimevolley_slimevolleyv0_nfsp'
 # yaml_file = 'confs/gym_cartpolev1_nfsp'
-yaml_file = 'confs/lasertag_LaserTagsmall2v0_nfsp'
+# yaml_file = 'confs/lasertag_LaserTagsmallv0_nfsp'
 
 args = LoadYAML2Dict(yaml_file, toAttr=True, mergeDefault=True)
+if parser_args.test:
+    args.test = True
+    args.render = True
+    args.load_model_idx = parser_args.model
+    args.eta = 0.
 
 ### Create env
 env = make_env(args)
