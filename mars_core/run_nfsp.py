5a1e75229c8e1647dcd14792961f05a41b77c6ef
quantumiracle <1402434478@qq.com>
2021-09-06 15:06:18 -0400

    test nfsp

--- mars_core/run_nfsp.py
+++ mars_core/run_nfsp.py
@@ -14,9 +14,9 @@ parser_args = parser.parse_args()
 
 ### Load configurations
 # yaml_file = 'confs/pettingzoo_boxingv1_nfsp'
-#yaml_file = 'confs/slimevolley_slimevolleyv0_nfsp'
+yaml_file = 'confs/slimevolley_slimevolleyv0_nfsp'
 # yaml_file = 'confs/gym_cartpolev1_nfsp'
-yaml_file = 'confs/lasertag_LaserTagsmallv0_nfsp'
+# yaml_file = 'confs/lasertag_LaserTagsmallv0_nfsp'
 
 args = LoadYAML2Dict(yaml_file, toAttr=True, mergeDefault=True)
 if parser_args.test:
