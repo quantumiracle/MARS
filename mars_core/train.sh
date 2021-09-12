5cc96dc4723ac12ef295d220b8645dd92f242285
zihan <zhding@mail.ustc.edu.cn>
2021-09-12 18:55:35 -0400

    res

--- mars_core/train.sh
+++ mars_core/train.sh
@@ -6,5 +6,5 @@ echo "Save as: " $DATE
 # nohup python exploit_nash_exploiter.py > $DATE$RAND.log &
 # nohup python run_nfsp.py  > $DATE$RAND.log 2>&1 &
 # nohup python run_nash_dqn_exploiter.py > $DATE$RAND.log &
-#nohup python run.py > $DATE$RAND.log &
-nohup python run_nxdo.py > $DATE$RAND.log &
+nohup python run.py > $DATE$RAND.log &
+#nohup python run_nxdo.py > $DATE$RAND.log &
