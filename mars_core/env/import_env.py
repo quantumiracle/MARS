512781170a77a98dbb2514257d6a5e62d7c9a1e2
quantumiracle <1402434478@qq.com>
2021-09-23 16:25:30 -0400

    fix bug fsp in pettingzoo atari

--- mars_core/env/import_env.py
+++ mars_core/env/import_env.py
@@ -109,7 +109,7 @@ def _create_single_env(env_name: str, env_type: str, args: Dict):
 
             # initialize the env
             env = eval(env_name).parallel_env(obs_type=obs_type)
-            env = supersuit.agent_indicator_v0(env) # for selfplay, agent from two sides can use the same model; see https://github.com/PettingZoo-Team/PettingZoo/issues/423
+            env = supersuit.agent_indicator_v0(env) # for selfplay, agent from two sides can use the same model (but it needs to see samples from two sides); see https://github.com/PettingZoo-Team/PettingZoo/issues/423
             env_agents = env.unwrapped.agents  # this cannot go through supersuit wrapper, so get it first and reassign it
 
             # assign necessary wrappers
