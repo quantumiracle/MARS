ba15d759cb9d11785fde1924fd512e31aaed9497
quantumiracle <1402434478@qq.com>
2021-09-23 16:25:30 -0400

    fix bug fsp in pettingzoo atari

--- mars_core/run.py
+++ mars_core/run.py
@@ -3,6 +3,14 @@ from env.import_env import make_env
 from rollout import rollout
 from rl.algorithm import *
 
+import argparse
+parser = argparse.ArgumentParser(
+    description='NFSP training.')
+parser.add_argument('--test', dest='test', action='store_true', default=False)
+parser.add_argument('--model', type=str, default=None,
+                    help='model index')
+parser_args = parser.parse_args()
+
 ### Load configurations
 # yaml_file = 'confs/pettingzoo_pongv1_selfplay_dqn'
 # yaml_file = 'confs/pettingzoo_boxingv1_selfplay_dqn' 
@@ -12,9 +20,12 @@ from rl.algorithm import *
 # yaml_file = 'confs/slimevolley_slimevolleyv0_fictitiousselfplay_dqn'
 yaml_file = 'confs/pettingzoo_boxingv1_fictitiousselfplay_dqn'
 
-
 args = LoadYAML2Dict(yaml_file, toAttr=True, mergeDefault=True)
-
+if parser_args.test:
+    args.test = True
+    args.render = True
+    args.load_model_full_path = '../model/pettingzoo_boxing_v1_fictitious_selfplay_DQN_20210921162239/9971'
+    
 ### Create env
 env = make_env(args)
 print(env)
