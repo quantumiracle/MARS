20a55f95bd153c8b00e4dcdf99f5f3f36d733449
quantumiracle <1402434478@qq.com>
2021-09-23 19:04:29 -0400

    add nxdo 2 side version

--- mars_core/marl/double_oracle.py
+++ mars_core/marl/double_oracle.py
@@ -3,7 +3,7 @@ import numpy as np
 from datetime import datetime
 import sys
 sys.path.append("..")
-from rl.algorithm.equilibrium_solver import NashEquilibriumECOSSolver
+from rl.algorithm.equilibrium_solver import NashEquilibriumECOSSolver, NashEquilibriumCVXPYSolver
 
 class NXDOMetaLearner():
     """
@@ -91,7 +91,7 @@ class NXDOMetaLearner():
         For example, if current evaluation matrix is:
         0  1
         -1 0
-        After adding a new policy with evaluated average episode reward (a,b) against current 2 policy, 
+        After adding a new policy with evaluated average episode reward (a,b) against current two policies, 
         it gives:
         0   1 -a
         -1  0 -b
@@ -134,3 +134,128 @@ class NXDOMetaLearner():
         avg_epi_rewards = avg_epi_rewards/eval_episodes
 
         return avg_epi_rewards
+
+
+class NXDO2SideMetaLearner(NXDOMetaLearner):
+    """
+    Meta learn is the  for MARL meta strategy, 
+    which assigns the policy update schedule on a level higher
+    than policy update itself in standard RL.
+
+    Neural Extensive-Form Double Oracle (NXDO) 
+
+    Ref: https://arxiv.org/pdf/2103.06426.pdf
+    """
+    def __init__(self, logger, args, save_checkpoint=True):
+        # super(NXDO2SideMetaLearner, self).__init__(logger, args, save_checkpoint)
+        self.model_path = logger.model_dir
+
+        # get names
+        # self.model_name = logger.keys[args.marl_spec['trainable_agent_idx']]
+        # self.opponent_name = logger.keys[args.marl_spec['opponent_idx']]
+
+        self.current_learnable_model_idx = 0
+        self.current_fixed_opponent_idx = 1
+
+        self.save_checkpoint = save_checkpoint
+        self.args = args
+        self.last_update_epi= 0
+        self.saved_checkpoints = [[] for _ in range(2)] # for both player
+        self.evaluation_matrix = np.array([])  # the evaluated utility matrix (N*N) of policy league with N policies
+        self.nash_meta_strategy = None
+        logger.add_extr_log('matrix_equilibrium')
+
+    def _switch_charac(self,):
+        """ Iteratively calculate the meta-Nash equilibrium and learn the best response, so switch the characters after each update."""
+        idx = self.current_learnable_model_idx
+        self.current_learnable_model_idx = self.current_fixed_opponent_idx 
+        self.current_fixed_opponent_idx = idx
+
+    def step(self, model, logger, env, args, min_update_interval = 20):
+        """
+        A meta learner step (usually at the end of each episode), update models if available.
+
+        params: 
+            :min_update_interval: mininal opponent update interval in unit of episodes
+        """
+        # score_avg_window = self.args.log_avg_window # use the same average window as logging for score delta
+        score_avg_window = 10 # use the same average window as logging for score delta
+
+        score_delta = np.mean(logger.epi_rewards[logger.keys[self.current_learnable_model_idx]][-score_avg_window:])\
+             - np.mean(logger.epi_rewards[logger.keys[self.current_fixed_opponent_idx]][-score_avg_window:])
+
+        # this is an indicator that best response policy is found
+        if score_delta  > self.args.marl_spec['selfplay_score_delta']\
+             and logger.current_episode - self.last_update_epi > min_update_interval:
+            # update the opponent with current model, assume they are of the same type
+            if self.save_checkpoint:
+                model.agents[self.current_learnable_model_idx].save_model(self.model_path+str(logger.keys[self.current_learnable_model_idx])+'_'+str(logger.current_episode)) # save all checkpoints
+                self.saved_checkpoints[self.current_learnable_model_idx].append(str(logger.current_episode))
+
+            logger.additional_logs.append(f'Score delta: {score_delta}, udpate {logger.keys[self.current_learnable_model_idx]}.')
+            self.last_update_epi = logger.current_episode
+
+            ### update the opponent with epsilon meta Nash policy
+            # evaluate the N*N utility matrix, N is the number of currently saved models
+            added_row = []
+            if len(self.saved_checkpoints[self.current_fixed_opponent_idx]) >= 1:
+                agents = model.Kwargs['eval_models']
+                env = model.Kwargs['eval_env']
+                agents[0].load_model(self.model_path+str(logger.keys[self.current_learnable_model_idx])+'_'+self.saved_checkpoints[self.current_learnable_model_idx][-1]) # current model
+                for opponent_model_id in self.saved_checkpoints[self.current_fixed_opponent_idx]:
+                    agents[1].load_model(self.model_path+str(logger.keys[self.current_fixed_opponent_idx])+'_'+opponent_model_id)
+                    added_row.append(self.evaluate(env, agents, args)[0])  # reward for current learnable model
+                # print('row: ', added_row, self.current_learnable_model_idx)
+                self.update_matrix(self.current_learnable_model_idx, np.array(added_row)) # add new evaluation results to matrix
+                # print('matrix: ', self.evaluation_matrix)
+                if len(self.saved_checkpoints[self.current_fixed_opponent_idx])*len(self.saved_checkpoints[self.current_fixed_opponent_idx]) >= 4: # no need for NE when (1,1), (1,2)
+                    # rollout with NFSP to learn meta strategy or directly calculate the Nash from the matrix
+                    # self.nash_meta_strategy = NashEquilibriumECOSSolver(self.evaluation_matrix) # present implementation cannot solve non-square matrix
+                    self.nash_meta_strategy = NashEquilibriumCVXPYSolver(self.evaluation_matrix) # cvxpy can solve non-square matrix, just a bit slower, but nxdo doesn't solve Nash often
+                    # the solver returns the equilibrium strategies for both players, just take one; it should be the same due to the symmetric poicy space
+                    self.nash_meta_strategy = self.nash_meta_strategy[self.current_learnable_model_idx]
+                    # print('nash: ', self.nash_meta_strategy)
+                    logger.extr_logs.append(f'Current episode: {logger.current_episode}, utitlity matrix: {self.evaluation_matrix}, Nash stratey: {self.nash_meta_strategy}')
+
+            self._switch_charac()
+
+            # change learnable and not learnable to achieve iterative learning
+            if not self.current_fixed_opponent_idx in model.not_learnable_list:
+                model.not_learnable_list.append(self.current_fixed_opponent_idx)
+            if self.current_learnable_model_idx in model.not_learnable_list:
+                model.not_learnable_list.remove(self.current_learnable_model_idx)
+
+        # sample from Nash meta policy in a episode-wise manner
+        if len(self.saved_checkpoints[self.current_fixed_opponent_idx])*len(self.saved_checkpoints[self.current_fixed_opponent_idx]) >= 4 and self.nash_meta_strategy is not None:
+            sample_hist = np.random.multinomial(1, self.nash_meta_strategy)
+            policy_idx = np.squeeze(np.where(sample_hist>0))
+            # print('points: ', self.saved_checkpoints, policy_idx)
+            model.agents[self.current_fixed_opponent_idx].load_model(self.model_path+str(logger.keys[self.current_fixed_opponent_idx])+'_'+self.saved_checkpoints[self.current_fixed_opponent_idx][policy_idx])
+
+    def update_matrix(self, idx, row):
+        """
+        Adding a new policy to the league will add a row or column to the present evaluation matrix, after it has
+        been evaluated against all policies in the opponent's league. Whether adding a row or column depends on whether
+        it is the row player (idx=0) or column player (idx=1). 
+        For example, if current evaluation matrix is:
+        2  1
+        -1 3
+        After adding a new policy for row player with evaluated average episode reward (a,b) against current two policies in 
+        the league of the column player, it gives: 
+        it gives:
+        2   1
+        -1  3
+        a   b    
+        """ 
+        if idx == 0: # for row player
+            if self.evaluation_matrix.shape[0] == 0: 
+                self.evaluation_matrix = np.array([row])
+            else:  # add row
+                self.evaluation_matrix = np.vstack([self.evaluation_matrix, row])
+        elif idx == 1: # for column player
+            if self.evaluation_matrix.shape[0] == 0:  # the first update
+                self.evaluation_matrix = -np.array([row])  # the minus is due to evaluation matrix is always from the first player's perspective, but not necessarily the current_learnable_model
+            else:  # add column
+                self.evaluation_matrix=np.hstack([self.evaluation_matrix, -np.array([row]).T]) # same reason for minus
+
+        return self.evaluation_matrix
---
+++ mars_core/run_nxdo2.py
@@ -0,0 +1,30 @@
+from utils.func import LoadYAML2Dict
+from env.import_env import make_env
+from rollout import rollout
+from rl.algorithm import *
+
+### Load configurations
+yaml_file = 'confs/slimevolley_slimevolleyv0_nxdo2_dqn'
+# yaml_file = 'confs/pettingzoo_boxingv1_nxdo2_dqn'
+
+
+args = LoadYAML2Dict(yaml_file, toAttr=True, mergeDefault=True)
+
+### Create env
+env = make_env(args)
+print(env)
+
+### Specify models for each agent
+model1 = eval(args.algorithm)(env, args)
+model2 = eval(args.algorithm)(env, args)
+# model1.fix()  # fix a model if you don't want it to learn
+
+args.num_envs = 1 # create a single evaluation env
+eval_env = make_env(args)
+eval_model1 = eval(args.algorithm)(env, args)
+eval_model2 = eval(args.algorithm)(env, args)
+
+model = MultiAgent(env, [model1, model2], args, eval_models = [eval_model1, eval_model2], eval_env = eval_env)
+
+### Rollout
+rollout(env, model, args)
