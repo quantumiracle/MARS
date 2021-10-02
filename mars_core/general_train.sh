11e438db7524cfc3373e5517de01d129171f140a
quantumiracle <zhding@mail.ustc.edu.cn>
2021-10-02 16:52:19 -0400

    fix

--- mars_core/general_train.sh
+++ mars_core/general_train.sh
@@ -3,7 +3,7 @@ echo "Running DATE:" $(date +"%Y-%m-%d %H:%M")
 DATE=`date '+%Y%m%d_%H%M'`
 
 declare -a envs=('pettingzoo_boxingv1')
-declare -a methods=('selfplay', 'fictitiousselfplay', 'nxdo', 'nxdo2')
+declare -a methods=('selfplay' 'fictitiousselfplay' 'nxdo' 'nxdo2')
 
 for env in ${envs[@]}; do
     for method in ${methods[@]}; do
