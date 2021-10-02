11537b23fe4b3feada9efb1350559339fe196673
quantumiracle <zhding@mail.ustc.edu.cn>
2021-10-02 17:20:10 -0400

    fix

--- mars_core/general_train.sh
+++ mars_core/general_train.sh
@@ -7,7 +7,7 @@ declare -a methods=('selfplay' 'fictitiousselfplay' 'nxdo' 'nxdo2')
 
 for env in ${envs[@]}; do
     for method in ${methods[@]}; do
-        echo python general_run.py --env $env --method $method > $DATE$RANDOM.log &
+        echo python general_run.py --env $env --method $method &
         nohup python general_run.py --env $env --method $method > $DATE$RANDOM.log &
     done
 done
