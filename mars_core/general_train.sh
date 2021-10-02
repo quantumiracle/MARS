e06bbc2cc56d783b317128ef7efbbc26c27aa5fd
quantumiracle <zhding@mail.ustc.edu.cn>
2021-10-02 16:50:15 -0400

    test

--- mars_core/general_train.sh
+++ mars_core/general_train.sh
@@ -1,14 +1,13 @@
 echo "Running DATE:" $(date +"%Y-%m-%d %H:%M")
 
 DATE=`date '+%Y%m%d_%H%M'`
-echo "Save as: " $DATE
 
 declare -a envs=('pettingzoo_boxingv1')
 declare -a methods=('selfplay', 'fictitiousselfplay', 'nxdo', 'nxdo2')
 
 for env in ${envs[@]}; do
     for method in ${methods[@]}; do
-        echo python general_run.py --env $env --method $method > $DATE$RAND.log
-        nohup python general_run.py --env $env --method $method > $DATE$RAND.log &
+        echo python general_run.py --env $env --method $method > $DATE$RANDOM.log &
+        nohup python general_run.py --env $env --method $method > $DATE$RANDOM.log &
     done
 done
