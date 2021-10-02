14df364072f4a3716b2223530c21d23986478797
quantumiracle <zhding@mail.ustc.edu.cn>
2021-10-02 17:24:34 -0400

    t

---
+++ mars_core/general_train2.sh
@@ -0,0 +1,13 @@
+echo "Running DATE:" $(date +"%Y-%m-%d %H:%M")
+
+DATE=`date '+%Y%m%d_%H%M'`
+
+declare -a envs=('slimevolley_slimevolleyv0')
+declare -a methods=('fictitiousselfplay' 'nxdo2')
+
+for env in ${envs[@]}; do
+    for method in ${methods[@]}; do
+        echo python general_run.py --env $env --method $method &
+        nohup python general_run.py --env $env --method $method > $DATE$RANDOM.log &
+    done
+done
