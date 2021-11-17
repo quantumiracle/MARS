25ce5db19ba4def5697f86d23fe0c5e55efcf5cd
quantumiracle <zhding@mail.ustc.edu.cn>
2021-11-17 08:29:45 -0500

    local

--- mars_core/marl/meta_learner.py
+++ mars_core/marl/meta_learner.py
@@ -1,4 +1,4 @@
-from .selfplay import SelfPlayMetaLearner, FictitiousSelfPlayMetaLearner
+from .selfplay import SelfPlayMetaLearner, SelfPlay2SideMetaLearner, FictitiousSelfPlayMetaLearner
 from .double_oracle import NXDOMetaLearner, NXDO2SideMetaLearner
 
 class MetaLearner():
@@ -11,6 +11,9 @@ class MetaLearner():
 def init_meta_learner(logger, args, *kargs):
     if args.marl_method == 'selfplay':
         return SelfPlayMetaLearner(logger, args, *kargs)
+
+    if args.marl_method == 'selfplay2':
+        return SelfPlay2SideMetaLearner(logger, args, *kargs)
 
     if args.marl_method == 'fictitious_selfplay':
         return FictitiousSelfPlayMetaLearner(logger, args, *kargs)
---
+++ mars_core/train.py
@@ -0,0 +1,36 @@
+from utils.func import LoadYAML2Dict
+from env.import_env import make_env
+from rollout import rollout
+from rl.algorithm import *
+from general_train import get_general_args
+import argparse
+parser = argparse.ArgumentParser(description='Arguments of the general launching script for MARS.')
+
+### Load configurations
+game_type = 'pettingzoo'
+game = ['boxing_v1', 'surround_v1', 'combat_plane_v1'][0]
+method = ['selfplay', 'selfplay2', 'nash_dqn', 'nash_dqn_exploiter'][1]
+
+args = get_general_args(game_type+'_'+game, method)
+print(args)
+
+### Create env
+env = make_env(args)
+print(env)
+
+### Specify models for each agent     
+model1 = eval(args.algorithm)(env, args)
+model2 = eval(args.algorithm)(env, args)
+
+if method in ['nxdo', 'nxdo2']:
+    eval_env = make_env(args)
+    eval_model1 = eval(args.algorithm)(env, args)
+    eval_model2 = eval(args.algorithm)(env, args)
+
+    model = MultiAgent(env, [model1, model2], args, eval_models = [eval_model1, eval_model2], eval_env = eval_env)   
+
+else:
+    model = MultiAgent(env, [model1, model2], args)
+
+### Rollout
+rollout(env, model, args)
