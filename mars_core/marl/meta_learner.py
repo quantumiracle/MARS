7d4adb5c3e89a46b4ed56440a3ab86c9b6dcf9e6
quantumiracle <zhding@mail.ustc.edu.cn>
2021-11-17 23:23:36 -0500

    add selfplay fsp two side version

--- mars_core/marl/meta_learner.py
+++ mars_core/marl/meta_learner.py
@@ -1,4 +1,4 @@
-from .selfplay import SelfPlayMetaLearner, SelfPlay2SideMetaLearner, FictitiousSelfPlayMetaLearner
+from .selfplay import SelfPlayMetaLearner, SelfPlay2SideMetaLearner, FictitiousSelfPlayMetaLearner, FictitiousSelfPlay2SideMetaLearner
 from .double_oracle import NXDOMetaLearner, NXDO2SideMetaLearner
 
 class MetaLearner():
@@ -12,16 +12,19 @@ def init_meta_learner(logger, args, *kargs):
     if args.marl_method == 'selfplay':
         return SelfPlayMetaLearner(logger, args, *kargs)
 
-    if args.marl_method == 'selfplay2':
+    elif args.marl_method == 'selfplay2':
         return SelfPlay2SideMetaLearner(logger, args, *kargs)
 
-    if args.marl_method == 'fictitious_selfplay':
+    elif args.marl_method == 'fictitious_selfplay':
         return FictitiousSelfPlayMetaLearner(logger, args, *kargs)
 
-    if args.marl_method == 'nxdo':
+    elif args.marl_method == 'fictitious_selfplay2':
+        return FictitiousSelfPlay2SideMetaLearner(logger, args, *kargs)
+
+    elif args.marl_method == 'nxdo':
         return NXDOMetaLearner(logger, args, *kargs)
 
-    if args.marl_method == 'nxdo2':
+    elif args.marl_method == 'nxdo2':
         return NXDO2SideMetaLearner(logger, args, *kargs)
 
     else:
