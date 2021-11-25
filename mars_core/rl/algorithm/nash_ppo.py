f5c1a6cd427459cd0da0aec9fec478d398ebb4db
quantumiracle <zhding@mail.ustc.edu.cn>
2021-11-25 11:18:00 -0500

    fix nash ppo

--- mars_core/rl/algorithm/nash_ppo.py
+++ mars_core/rl/algorithm/nash_ppo.py
@@ -1,4 +1,5 @@
 import copy
+from math import log
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,22 +11,8 @@ from .common.agent import Agent
 from .common.rl_utils import choose_optimizer
 from utils.typing import List, Tuple, StateType, ActionType, SampleType, SingleEnvMultiAgentSampleType
 
-def PPO(env, args):
-    """ The function returns a proper class for Proximal Policy Optimization (PPO) algorithm,
-    according to the action space of the environment.
-
-    :param env: environment instance
-    :type env: object
-    :param args: arguments
-    :type args: ConfigurationDict
-    """    
-    if True: # discrete TODO
-        return PPODiscrete(env, args)
-    else:
-        return None
-
-class PPODiscrete(Agent):
-    """ PPO agorithm for environments with discrete action space.
+class NashPPO(Agent):
+    """ Nash-PPO agorithm for environments with discrete action space.
     """ 
     def __init__(self, env, args):
         super().__init__(env, args)
@@ -54,13 +41,16 @@ class PPODiscrete(Agent):
                 self.policies.append(CNN(env.observation_space, env.action_space, args.net_architecture['policy'], model_for='discrete_policy').to(self.device))
                 self.values.append(CNN(env.observation_space, env.action_space, args.net_architecture['value'], model_for='value').to(self.device))
 
+                self.common_layers = CNN(env.observation_space, env.action_space, args.net_architecture['value'], model_for='value').to(self.device)           
 
-        # cannot use lambda in multiprocessing
-        # self.pi = lambda x: self.policy.forward(x, softmax_dim=-1)
-        # self.v = lambda x: self.value.forward(x)            
+        policy_params, value_params = [], []
+        for p, v in zip(self.policies, self.values):
+            policy_params += list(p.parameters())
+            value_params += list(v.parameters())
 
         # TODO a single optimizer for two nets may be problematic
-        self.optimizer = choose_optimizer(args.optimizer)(list(self.value.parameters())+list(self.policy.parameters()), lr=float(args.learning_rate))
+        self.optimizer = choose_optimizer(args.optimizer)(policy_params+value_params, lr=float(args.learning_rate))
+        self.common_layer_optimizer = choose_optimizer(args.optimizer)(self.common_layers.parameters(), lr=float(args.learning_rate))
         self.mseLoss = nn.MSELoss()
         self._num_channel = args.num_envs*(env.num_agents if isinstance(env.num_agents, int) else env.num_agents[0]) # env.num_agents is a list when using parallel envs 
         self.data = [[] for _ in range(self._num_channel)]
@@ -127,15 +117,25 @@ class PPODiscrete(Agent):
         :return: the actions
         :rtype: List[ActionType]
         """
-        prob = self.policy(torch.from_numpy(s).unsqueeze(0).float().to(self.device)).squeeze()  # make sure input state shape is correct
+        actions = []
+        logprobs = []
         if Greedy:
-            a = torch.argmax(prob, dim=-1).detach().cpu().numpy()
-            return a
+            for policy, state_per_agent in zip(self.policies, s):
+                prob = policy(torch.from_numpy(state_per_agent).unsqueeze(0).float().to(self.device)).squeeze()  # make sure input state shape is correct
+                a = torch.argmax(prob, dim=-1).detach().cpu().numpy()
+                actions.append(a)
+            return actions
         else:
-            dist = Categorical(prob)
-            a = dist.sample()
-            logprob = dist.log_prob(a)
-            return a.detach().cpu().numpy(), logprob.detach().cpu().numpy()
+            for policy, state_per_agent in zip(self.policies, s):
+                prob = policy(torch.from_numpy(state_per_agent).unsqueeze(0).float().to(self.device)).squeeze()  # make sure input state shape is correct
+                dist = Categorical(prob)
+                a = dist.sample()
+                logprob = dist.log_prob(a)
+                actions.append(a.detach().cpu().numpy())
+                logprobs.append(logprob.detach().cpu().numpy())
+            actions = np.array(actions)
+            logprobs = np.array(logprobs)
+            return actions, logprobs
         
     def make_batch(
         self, 
@@ -175,16 +175,17 @@ class PPODiscrete(Agent):
             s, a, r, s_prime, oldlogprob, done_mask = self.make_batch(data)
             
             # need to prcess the samples, separate for agents
-
-
+            s_ = s.view(s.shape[0], 2, -1)
+            s_prime_ = s_prime.view(s_prime.shape[0], 2, -1)
+            losses = 0.
+            common_layer_losses = 0.
             for _ in range(self.K_epoch):
                 # loss for each agent in standard PPO
-                ratio_list = []  # record the policy ratio for each agent
                 for i in range(2):
-                    vs = self.v(s, i)
+                    vs = self.v(s_[:, i, :], i)  # take the state for the specific agent
 
                     # use generalized advantage estimation
-                    vs_prime = self.v(s_prime, i).squeeze(dim=-1)
+                    vs_prime = self.v(s_prime_[:, i, :], i).squeeze(dim=-1)
                     assert vs_prime.shape == done_mask.shape
                     vs_target = r + self.gamma * vs_prime * done_mask  # TODO r-> r[i]
                     delta = vs_target - vs.squeeze(dim=-1)
@@ -198,27 +199,35 @@ class PPODiscrete(Agent):
                     advantage = torch.tensor(advantage_lst, dtype=torch.float).to(self.device)
                     advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-5)  # this can have significant improvement (efficiency, stability) on performance
 
-                    pi = self.pi(s, i)
+                    pi = self.pi(s_[:, i, :], i)
                     dist = Categorical(pi)
                     dist_entropy = dist.entropy()
-                    logprob = dist.log_prob(a)
-                    # pi_a = pi.gather(1,a)
-                    # ratio = torch.exp(torch.log(pi_a) - torch.log(prob_a))  # a/b == exp(log(a)-log(b))
-                    ratio = torch.exp(logprob - oldlogprob)
-                    ratio_list.append(ratio)
+                    logprob = dist.log_prob(a[:, i])
+                    ratio = torch.exp(logprob - oldlogprob[:, i])
                     surr1 = ratio * advantage
                     surr2 = torch.clamp(ratio, 1-self.eps_clip, 1+self.eps_clip) * advantage
-                    loss = -torch.min(surr1, surr2) + F.smooth_l1_loss(vs.squeeze(dim=-1) , vs_target.detach()) - 0.01*dist_entropy
-                
+                    ppo_loss = -torch.min(surr1, surr2) + F.mse_loss(vs.squeeze(dim=-1) , vs_target.detach()) - 0.01*dist_entropy  # TODO vec + scalar + vec, is this valid?
+                    ppo_loss = ppo_loss.mean()
+
                     # loss for common layers (value function)
-                    vs_prime = self.common_layers(s_prime).squeeze(dim=-1)
+                    vs_prime = self.common_layers(s_prime_[:, 0, :]).squeeze(dim=-1)  # TODO
                     vs_target = r + self.gamma * vs_prime * done_mask
-                    vs = self.common_layers(s)
-                    loss = F.smooth_l1_loss(vs.squeeze(dim=-1) , vs_target.detach())
+                    vs = self.common_layers(s_[:, 0, :])  # TODO
+                    common_layer_loss = F.mse_loss(vs.squeeze(dim=-1) , vs_target.detach()).mean()
+
+                    self.optimizer.zero_grad()
+                    ppo_loss.backward()
+                    self.optimizer.step()
+
+                    self.common_layer_optimizer.zero_grad()
+                    common_layer_loss.backward()
+                    self.common_layer_optimizer.step()
+
+                    total_loss += ppo_loss.item() + common_layer_loss.item()
 
                 # nash loss for two agents policies, using the nash value
-                vs = self.common_layers(s)
-                vs_prime = self.common_layers(s_prime).squeeze(dim=-1)
+                vs = self.common_layers(s_[:, 0, :])  # TODO common layer may take a concatenation of states from two sides
+                vs_prime = self.common_layers(s_prime_[:, 0, :]).squeeze(dim=-1) # TODO
                 assert vs_prime.shape == done_mask.shape
                 vs_target = r + self.gamma * vs_prime * done_mask  # TODO r is player 1's here
                 delta = vs_target - vs.squeeze(dim=-1)
@@ -231,40 +240,68 @@ class PPODiscrete(Agent):
                 advantage_lst.reverse()
                 advantage = torch.tensor(advantage_lst, dtype=torch.float).to(self.device)
                 advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-5)  # this can have significant improvement (efficiency, stability) on performance
+                
+                # TODO can the following be written once? (error in previous trial)
+                ratio_list = []
+                for i in range(2): # get the ratio for both
+                    pi = self.pi(s_[:, i, :], i)
+                    dist = Categorical(pi)
+                    dist_entropy = dist.entropy()
+                    logprob = dist.log_prob(a[:, i])
+                    ratio = torch.exp(logprob - oldlogprob[:, i])
+                    ratio_list.append(ratio)  # the ratios need to be newly computed to have policy gradients
+                
                 surr1 = ratio_list[0] * ratio_list[1] * advantage
                 surr2 = torch.clamp(ratio_list[0] * ratio_list[1], 1-self.eps_clip, 1+self.eps_clip)
-                policy_loss1 = -torch.min(surr1, surr2)
-                policy_loss2 = torch.min(surr1, surr2)
-
-                # sum all the loss and backpropagate
+                policy_loss1 = -torch.min(surr1, surr2).mean()
 
                 self.optimizer.zero_grad()
-                mean_loss = loss.mean()
-                mean_loss.backward()
+                policy_loss1.backward()
                 self.optimizer.step()
 
-            total_loss += mean_loss.item()
+                ratio_list = []
+                for i in range(2): # get the ratio for both
+                    pi = self.pi(s_[:, i, :], i)
+                    dist = Categorical(pi)
+                    dist_entropy = dist.entropy()
+                    logprob = dist.log_prob(a[:, i])
+                    ratio = torch.exp(logprob - oldlogprob[:, i])
+                    ratio_list.append(ratio)  # the ratios need to be newly computed to have policy gradients
+                
+                surr1 = ratio_list[0] * ratio_list[1] * advantage
+                surr2 = torch.clamp(ratio_list[0] * ratio_list[1], 1-self.eps_clip, 1+self.eps_clip)
+                policy_loss2 = torch.min(surr1, surr2).mean()
+                
+                self.optimizer.zero_grad()
+                policy_loss2.backward()
+                self.optimizer.step()
+
+                loss = policy_loss1 + policy_loss2
+
+                total_loss += loss.item()
 
         self.data = [[] for _ in range(self._num_channel)]
 
         # Copy new weights into old policy:
-        self.policy_old.load_state_dict(self.policy.state_dict())
         return total_loss
 
     def save_model(self, path=None):
         try:  # for PyTorch >= 1.7 to be compatible with loading models from any lower version
-            torch.save(self.policy.state_dict(), path+'_policy', _use_new_zipfile_serialization=False)
-            torch.save(self.value.state_dict(), path+'_value', _use_new_zipfile_serialization=False)
+            for i, (pi, v) in enumerate(zip(self.policies, self.values)):
+                torch.save(pi.state_dict(), path+f'_{i}_policy', _use_new_zipfile_serialization=False)
+                torch.save(v.state_dict(), path+f'_{i}_value', _use_new_zipfile_serialization=False)
+            torch.save(self.common_layers.state_dict(), path+f'_common', _use_new_zipfile_serialization=False)
         except:
-            torch.save(self.policy.state_dict(), path+'_policy')
-            torch.save(self.value.state_dict(), path+'_value')
-
+            for i, (pi, v) in enumerate(zip(self.policies, self.values)):
+                torch.save(pi.state_dict(), path+f'_{i}_policy')
+                torch.save(v.state_dict(), path+f'_{i}_value')
+            torch.save(self.common_layers.state_dict(), path+f'_common')
 
     def load_model(self, path=None):
-        self.policy.load_state_dict(torch.load(path+'_policy'))
-        self.policy_old.load_state_dict(self.policy.state_dict())  # important
-
-        self.value.load_state_dict(torch.load(path+'_value'))
+        for i, (pi, v) in enumerate(zip(self.policies, self.values)):
+            pi.load_state_dict(torch.load(path+f'_{i}_policy'))
+            v.load_state_dict(torch.load(path+f'_{i}_value'))
+        self.common_layers.load_state_dict(torch.load(path+f'_common'))
 
 
 
