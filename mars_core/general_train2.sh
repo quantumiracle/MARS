cc10f9eb4d71f13d6a4b3cea8945acad88f41d2a
zihan <zhding@mail.ustc.edu.cn>
2021-12-06 10:15:54 -0500

    r

--- mars_core/general_train2.sh
+++ mars_core/general_train2.sh
@@ -1,11 +1,11 @@
 echo "Running DATE:" $(date +"%Y-%m-%d %H:%M")
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 DATE=`date '+%Y%m%d_%H%M'`
 echo "Save as: " $DATE
 
-declare -a envs=('pettingzoo_combat_tank_v1' 'pettingzoo_ice_hockey_v1')
-# declare -a envs=('pettingzoo_tennis_v2' 'pettingzoo_surround_v1')
+#declare -a envs=('pettingzoo_combat_tank_v1' 'pettingzoo_ice_hockey_v1')
+declare -a envs=('pettingzoo_tennis_v2' 'pettingzoo_surround_v1')
 declare -a methods=('selfplay2' 'fictitious_selfplay2' 'nxdo2' 'nfsp' 'nash_dqn' 'nash_dqn_exploiter' 'nash_ppo')
 mkdir -p log/$DATE
 
