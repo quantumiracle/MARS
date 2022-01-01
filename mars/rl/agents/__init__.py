410c35e0da2617b6d50867ebf4e1ba923a4285fe
quantumiracle <zhding@mail.ustc.edu.cn>
2022-01-01 17:11:29 -0500

    refact code structure

---
+++ mars/rl/agents/__init__.py
@@ -0,0 +1,9 @@
+from .dqn import DQN
+from .nash_dqn import NashDQN
+from .nash_dqn_exploiter import NashDQNExploiter
+from .nash_dqn_speed import NashDQNSpeed
+from .nash_ppo import NashPPO
+from .nfsp import NFSP
+from .ppo import PPO
+from .ga import GA
+from .agent import Agent, MultiAgent
--- mars_core/rl/algorithm/__init__.py
+++
@@ -1,9 +0,0 @@
-from .dqn import DQN
-from .nash_dqn import NashDQN
-from .nash_dqn_exploiter import NashDQNExploiter
-from .nash_dqn_speed import NashDQNSpeed
-from .nash_ppo import NashPPO
-from .nfsp import NFSP
-from .ppo import PPO
-from .ga import GA
-from .common.agent import Agent, MultiAgent
