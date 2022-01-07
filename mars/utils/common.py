12daea816814676eb3c7a8f31e69ba768c47eead
quantumiracle <zhding@mail.ustc.edu.cn>
2022-01-07 15:27:47 -0500

    clean deeper code

--- mars/utils/common.py
+++ mars/utils/common.py
@@ -1,11 +1,11 @@
 # These methods only store one-side models; otherwise two-side models are saved.
 SelfplayBasedMethods = ['selfplay', 'fictitious_selfplay', 'nxdo'] 
 
-# # These methods require an evaluation model.
-# EvaluationModelMethods = ['nxdo', 'nxdo2'] 
-
-# There methods have a meta strategy
+# These methods have a meta strategy
 MetaStrategyMethods = ['fictitious_selfplay', 'fictitious_selfplay2', 'nxdo', 'nxdo2'] 
 
-# There methods needs a meta step
+# These methods needs a meta step
 MetaStepMethods = ['selfplay', 'selfplay2', 'fictitious_selfplay', 'fictitious_selfplay2', 'nxdo', 'nxdo2'] 
+
+# These methods are based on Nash
+NashBasedMethods = ['nash_dqn', 'nash_dqn_exploiter', 'nash_ppo']
