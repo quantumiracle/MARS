61e3b1fc2b80dee5650cdb7c62d480e3a01493f2
quantumiracle <zhding@mail.ustc.edu.cn>
2022-01-11 21:28:58 -0500

    test nash ppo

--- general_train.sh
+++ general_train.sh
@@ -1,11 +1,12 @@
 echo "Running DATE:" $(date +"%Y-%m-%d %H:%M")
+export CUDA_VISIBLE_DEVICES=4
 
 DATE=`date '+%Y%m%d_%H%M'`
 echo "Save as: " $DATE
 
-declare -a envs=('pettingzoo_boxing_v1' 'pettingzoo_combat_plane_v1' 'pettingzoo_surround_v1')
+declare -a envs=('pettingzoo_boxing_v1' 'pettingzoo_pong_v2' 'pettingzoo_surround_v1' 'pettingzoo_tennis_v2')
 # declare -a methods=('selfplay' 'fictitiousselfplay' 'nxdo' 'nxdo2' 'nfsp' 'nash_dqn' 'nash_dqn_exploiter')
-declare -a methods=('selfplay' 'fictitious_selfplay' 'nxdo' 'nxdo2' 'nfsp' 'nash_dqn' 'nash_dqn_exploiter')
+declare -a methods=('nash_ppo')
 mkdir -p log/$DATE
 
 for env in ${envs[@]}; do
