359f8c521e875854f499654951229ece6bdba36e
quantumiracle <zhding@mail.ustc.edu.cn>
2022-01-16 21:36:25 -0500

    local update

--- mars/rollout.py
+++ mars/rollout.py
@@ -151,6 +151,8 @@ def rollout_normal(env, model, save_id, args: ConfigurationDict) -> None:
             and epi % args.save_interval == 0 \
             and args.marl_method in MetaStrategyMethods:
             meta_learner.save_model()
+        
+            meta_learner.save_model(logger.model_dir+f'{epi}_')
 
 ### Genetic algorithm uses a different way of rollout. ###
 
