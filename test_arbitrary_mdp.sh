8ea94e9aaaeeec2472c8d7e22a53e190cca82fd6
quantumiracle <zhding@mail.ustc.edu.cn>
2022-01-20 12:05:30 -0500

    t

--- test_arbitrary_mdp.sh
+++ test_arbitrary_mdp.sh
@@ -4,5 +4,5 @@ echo "Save as: " $DATE
 
 mkdir -p log/$DATE
 
-echo python test_arbitrary_mdp.py  output log to: log/$DATE/nash_dqn_exploiter.log 
-nohup python test_arbitrary_mdp.py > log/$DATE/nash_dqn_exploiter.log 
+echo python test_arbitrary_mdp.py  output log to: log/$DATE/nash_dqn_exploiter.log & 
+nohup python test_arbitrary_mdp.py > log/$DATE/nash_dqn_exploiter.log &
