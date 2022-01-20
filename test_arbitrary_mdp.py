bdc0c8182a77cb92e1801af68ba1a13c42c10393
quantumiracle <zhding@mail.ustc.edu.cn>
2022-01-20 12:01:42 -0500

    t

--- test_arbitrary_mdp.py
+++ test_arbitrary_mdp.py
@@ -14,7 +14,7 @@ parser_args = parser.parse_args()
 
 ### Load configurations
 
-method = ['fictitious_selfplay2', 'nash_dqn', 'nxdo2', 'nash_ppo'][-1]
+method = ['fictitious_selfplay2', 'nash_dqn', 'nash_dqn_exploiter', 'nxdo2', 'nash_ppo'][2]
 prefix = 'mars/confs/mdp/mdp_arbitrary_mdp_'
 yaml_file = prefix + method
 
