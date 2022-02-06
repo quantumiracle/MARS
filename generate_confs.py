c9a373f4bb0088f78d954e351b8f2bcf0facb173
quantumiracle <zhding@mail.ustc.edu.cn>
2022-02-05 22:35:11 -0500

    test nash multi step

--- generate_confs.py
+++ generate_confs.py
@@ -135,6 +135,7 @@ for game in two_player_zero_sum_games:
         conf['train_args']['max_episodes'] = 10000
         # some method specific confs
         if method in ['nash_dqn', 'nash_dqn_exploiter']:
+            conf['agent_args']['algorithm_spec']['multi_step'] = 5
             conf['train_args']['update_itr'] = 1. # 0.1
             conf['train_args']['marl_spec']['global_state'] = False
             if method == 'nash_dqn':
