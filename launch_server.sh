5f96d0f0be6c642f2e651500b0d091656b520abd
quantumiracle <zhding@mail.ustc.edu.cn>
2022-03-13 15:54:08 -0400

    t

--- launch_server.sh
+++ launch_server.sh
@@ -3,8 +3,8 @@ echo "Running DATE:" $(date +"%Y-%m-%d %H:%M")
 DATE=`date '+%Y%m%d_%H%M'`
 echo "Save as: " $DATE
 
-# declare -a envs=('pettingzoo_boxing_v1' 'pettingzoo_pong_v2' 'pettingzoo_surround_v1' 'pettingzoo_tennis_v2')
-declare -a envs=('pettingzoo_boxing_v1')
+# declare -a envs=('pettingzoo_boxing_v1' 'pettingzoo_pong_v2' 'pettingzoo_surround_v1' 'pettingzoo_tennis_v2' 'slimevolley_SlimeVolley-v0')
+declare -a envs=('slimevolley_SlimeVolley-v0')
 
 # declare -a methods=('selfplay2' 'fictitious_selfplay2' 'nxdo2' 'nfsp' 'nash_dqn' 'nash_dqn_exploiter')
 declare -a methods=('nash_dqn' 'nash_dqn_exploiter')
@@ -12,7 +12,7 @@ mkdir -p log/$DATE
 
 for i in ${!envs[@]}; do
     for j in ${!methods[@]}; do
-        echo CUDA_VISIBLE_DEVICES=1 python general_launch.py --env ${envs[$i]} --method ${methods[$j]} --save_id $DATE output log to: log/$DATE/${envs[$i]}_${methods[$j]}.log &
-        CUDA_VISIBLE_DEVICES=3 nohup python general_launch.py --env ${envs[$i]} --method ${methods[$j]} --save_id $DATE >> log/$DATE/${envs[$i]}_${methods[$j]}.log &
+        echo CUDA_VISIBLE_DEVICES=$j python general_launch.py --env ${envs[$i]} --method ${methods[$j]} --save_id $DATE output log to: log/$DATE/${envs[$i]}_${methods[$j]}.log &
+        CUDA_VISIBLE_DEVICES=$j nohup python general_launch.py --env ${envs[$i]} --method ${methods[$j]} --save_id $DATE >> log/$DATE/${envs[$i]}_${methods[$j]}.log &
     done
 done
--- tutorial/solver/compare_solvers.py
+++ tutorial/solver/compare_solvers.py
@@ -5,9 +5,9 @@ from mars.env.mdp import ArbitraryMDP, MDPWrapper
 import numpy as np
 
 def create_test_env():
-    num_states = 3
-    num_actions_per_player = 3
-    num_trans = 3
+    num_states = 4
+    num_actions_per_player = 4
+    num_trans = 4
 
     env = MDPWrapper(ArbitraryMDP(num_states=num_states, num_actions_per_player=num_actions_per_player, num_trans=num_trans))
     trans_matrices = env.env.trans_prob_matrices # shape: [dim_transition, dim_state, dim_action (p1*p2), dim_state]
@@ -18,10 +18,13 @@ def create_test_env():
 
     return env
 
-solvers = ['DoubleOralce', 'OracleDoubleOralce', 'FictitiousSelfPlay', 'OracleFictitiousSelfPlay'][-1]
-
+solvers = ['OracleDoubleOralce', 'QLearningDoubleOralce', 'OracleFictitiousSelfPlay', 'QLearningFictitiousSelfPlay', 'DoubleOralce', 'FictitiousSelfPlay']
+episodes = [1000, 10000, 1000, 10000, 1000, 10000]
 env = create_test_env()
-save_path = f'./data/{solvers}/'
-os.makedirs(save_path, exist_ok=True)
-solver = eval(solvers)(env, save_path+'data.npy', solve_episodes=1000)
-solver.solve()
+
+for solver, epi in zip(solvers, episodes):
+    print(solver, epi)
+    save_path = f'./data/{solver}/'
+    os.makedirs(save_path, exist_ok=True)
+    s = eval(solver)(env, save_path+'data444.npy', solve_episodes=epi)
+    s.solve()
