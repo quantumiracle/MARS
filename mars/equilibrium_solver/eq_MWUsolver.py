fix numerical issue in mwu solver

mars/equilibrium_solver/eq_MWUsolver.py
@@ -8,6 +8,7 @@ https://dl.acm.org/doi/abs/10.1145/3219166.3219235?casa_token=4Vu0L6uBun8AAAAA:X
 """
 import numpy as np
 import time, copy
+import sys
 
 def get_payoff_vector(payoff_matrix, opponent_policy):
     payoff = opponent_policy @ payoff_matrix
@@ -16,6 +17,7 @@ def get_payoff_vector(payoff_matrix, opponent_policy):
 def NashEquilibriumMWUSolver(A, Itr=500, verbose=False):
     """ Solve Nash equilibrium with multiplicative weights udpate."""
     # discount = 0.9
+    EPS = 1e-7 # prevent numerical problem
     row_action_num = A.shape[0]
     col_action_num = A.shape[1]
     learning_rate = np.sqrt(np.log(row_action_num)/Itr)  # sqrt(log |A| / T)
@@ -27,20 +29,20 @@ def NashEquilibriumMWUSolver(A, Itr=500, verbose=False):
 
     for i in range(Itr):
         # for row player, maximizer
-        # payoff_vec = get_payoff_vector(A.T, policies[1])
-        payoff_vec = policies[1] @ A.T
+        policies_ = copy.deepcopy(policies)  # track old value before update (update is inplace)
+        payoff_vec = policies_[1] @ A.T
         policies[0] = policies[0] * np.exp(learning_rate*payoff_vec)
 
         # for col player, minimizer
         # payoff_vec = get_payoff_vector(A, policies[0])
-        payoff_vec = policies[0] @ A
+        payoff_vec = policies_[0] @ A
         policies[1] = policies[1] * np.exp(-learning_rate*payoff_vec)
 
         # above is unnormalized, normalize it to be distribution
         # for k in range(len(policies)):
         #     abs_policy = np.abs(policies[k])
         #     policies[k] = abs_policy/np.sum(abs_policy)
-        policies = policies/np.expand_dims(np.sum(policies, axis=-1), -1)
+        policies = policies/np.expand_dims(EPS+np.sum(policies, axis=-1), -1)
 
         # MWU is average-iterate coverging, so accumulate polices
         final_policy += policies
@@ -59,8 +61,9 @@ def NashEquilibriumMWUSolver(A, Itr=500, verbose=False):
     return final_policy, nash_value
 
 
-def NashEquilibriumParallelMWUSolver(A, Itr=100, verbose=False):
+def NashEquilibriumParallelMWUSolver(A, Itr=500, verbose=False):
     """ Solve mulitple Nash equilibrium with multiplicative weights udpate."""
+    EPS = 1e-7 # prevent numerical problem
     A = np.array(A)
     matrix_num = A.shape[0]
     row_action_num = A.shape[1]
@@ -74,17 +77,17 @@ def NashEquilibriumParallelMWUSolver(A, Itr=100, verbose=False):
 
     for i in range(Itr):
         # for row player, maximizer
-        payoff_vec = np.einsum('nb,nab->na', policies[:, 1], A) 
+        policies_ = copy.deepcopy(policies)  # track old value before update (update is inplace)
+        payoff_vec = np.einsum('nb,nab->na', policies_[:, 1], A) 
         policies[:, 0] = policies[:, 0] * np.exp(learning_rate*payoff_vec)
 
         # for col player, minimizer
-        payoff_vec = np.einsum('na,nab->nb', policies[:, 0], A) 
+        payoff_vec = np.einsum('na,nab->nb', policies_[:, 0], A) 
         policies[:, 1] = policies[:, 1] * np.exp(-learning_rate*payoff_vec)
 
 
         # above is unnormalized, normalize it to be distribution
-        policies = policies/np.expand_dims(np.sum(policies, axis=-1), -1)
-
+        policies = policies/np.expand_dims(EPS+np.sum(policies, axis=-1), -1)
         # MWU is average-iterate coverging, so accumulate polices
         final_policy += policies
 
@@ -94,11 +97,10 @@ def NashEquilibriumParallelMWUSolver(A, Itr=100, verbose=False):
         print(f'For row player, strategy is {final_policy[:, 0]}')
         print(f'For column player, strategy is {final_policy[:, 1]}')
         print(learning_rate)
-
+    
     nash_value = np.einsum('nb,nb->n', np.einsum('na,nab->nb', policies[:, 0], A), final_policy[:, 1])
 
     return final_policy, nash_value
-
 
 if __name__ == "__main__":
     ###   TEST LP NASH SOLVER ###

mars/rl/agents/nash_dqn_factorized.py
@@ -100,7 +100,10 @@ class NashDQNFactorized(DQN):
             try: # nash computation may report error and terminate the process
                 actions, dists, ne_vs = self.compute_nash(q_values)
             except:
-                print("Invalid nash computation.")
+                print("Error: Invalid nash computation in choose_action function.")
+                actions = np.random.randint(self.action_dims, size=(state.shape[0], self.num_agents))
+            if np.isnan(actions).any():
+                print("Error: Nan action value in Nash computation is derived in the choose_action function.")
                 actions = np.random.randint(self.action_dims, size=(state.shape[0], self.num_agents))
 
             if DEBUG: ## test on arbitrary MDP
@@ -137,7 +140,7 @@ class NashDQNFactorized(DQN):
         all_dists, all_ne_values = NashEquilibriumParallelMWUSolver(q_tables)
 
         if update:
-            return all_dists, all_ne_values
+            return all_dists, all_ne_values #  Nash distributions, Nash values
         else:
             # Sample actions from Nash strategies
             for ne in all_dists:
@@ -146,14 +149,14 @@ class NashDQNFactorized(DQN):
                     try:
                         sample_hist = np.random.multinomial(1, dist)  # return one-hot vectors as sample from multinomial
                     except:
-                        print('Not a valid distribution from Nash equilibrium solution.')
+                        print('Error: Not a valid distribution from Nash equilibrium solution.')
                         print(sum(ne[0]), sum(ne[1]))
                         print(dist)
                     a = np.where(sample_hist>0)
                     actions.append(a)
                 all_actions.append(np.array(actions).reshape(-1))
 
-            return np.array(all_actions), all_dists, all_ne_values
+            return np.array(all_actions), all_dists, all_ne_values  # Nash actions, Nash distributions, Nash values
 
     def update(self):
         state, action, reward, next_state, done = self.buffer.sample(self.batch_size)
@@ -164,7 +167,7 @@ class NashDQNFactorized(DQN):
         reward = torch.FloatTensor(reward).to(self.device)
         done = torch.FloatTensor(np.float32(done)).to(self.device)
 
-        # invididual DQN loss
+        # invididual DQN loss calculation
         def DQN_loss(model, target, state, action, reward, next_state, done, multi_step):
             q = model(state)
             q = q.gather(1, action.unsqueeze(1)).squeeze(1)
@@ -173,6 +176,7 @@ class NashDQNFactorized(DQN):
             target_q = reward + (self.gamma ** multi_step) * next_q * (1 - done)
             loss = F.mse_loss(q, target_q.detach(), reduction='none')
             return loss.mean()
+
         a1 = action[:, 0]
         a2 = action[:, 1]
         q1_loss = DQN_loss(self.q_net_1, self.target_q_net_1, state, a1, reward, next_state, done, self.multi_step)
@@ -191,9 +195,17 @@ class NashDQNFactorized(DQN):
         q_value = q_values.gather(1, action_.unsqueeze(1)).squeeze(1)
         try: # nash computation may encounter error and terminate the process
             _, next_q_value = self.compute_nash(target_next_q_values, update=True)
+            if np.isnan(next_q_value).any():
+                import pdb; pdb.set_trace()
+
         except: 
-            print("Invalid nash computation.")
+            print("Error: Invalid nash computation in the update function.")
             next_q_value = np.zeros_like(reward)
+
+        if np.isnan(next_q_value).any():
+            print("Error: Nan Nash value in Nash computation is derived in the udpate function.")
+            next_q_value = np.zeros_like(reward)
+
         next_q_value  = torch.FloatTensor(next_q_value).to(self.device)
         expected_q_value = reward + (self.gamma ** self.multi_step) * next_q_value * (1 - done)
         nash_loss = F.mse_loss(q_value, expected_q_value.detach(), reduction='none')
@@ -270,7 +282,7 @@ class NashDQNFactorizedBase(DQNBase):
     def _construct_net(self, env, net_args):
             input_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape = self._observation_shape)
             output_space = gym.spaces.Discrete(self._action_shape)
-            if len(self._observation_shape) <= 1: # not 3d image
+            if len(self._observation_shape) <= 1: # not 2d image
                 self.net = get_model('mlp')(input_space, output_space, net_args, model_for='discrete_q')
             else:
                 self.net = get_model('cnn')(input_space, output_space, net_args, model_for='discrete_q')

mars/utils/logger2.py
@@ -190,16 +190,16 @@ class Logger(TestLogger):
         if  len(self.losses[self.keys[0]])>0:  # non-empty
             print(
                 f'Update itr: {self.current_itr}/{self.args.max_update_itr} ({100*self.current_itr/self.args.max_update_itr:.4f}%), \
-                    last time consumption/overall running time: {time_taken:.4f}s / {current_time-self.init_time:.4f} s'
+                    last time consumption/overall running time: {time_taken:.2f}s / {current_time-self.init_time:.2f} s'
             )
             for k in self.keys:
                 print(f"{k}: loss: {np.mean(self._nan_filter(self.losses[k][-self.avg_window:])):.4f}")
 
         elif len(self.epi_rewards[self.keys[0]])>0:  # non-empty
             print(
-                f'Process ID: {self.save_id}, episode: {self.current_episode}/{self.args.max_episodes} ({100*self.current_episode/self.args.max_episodes:.4f}%), \
+                f'Process ID: {self.process_id}, episode: {self.current_episode}/{self.args.max_episodes} ({100*self.current_episode/self.args.max_episodes:.4f}%), \
                     avg. length: {np.mean(self.epi_length[-self.avg_window:])},\
-                    last time consumption/overall running time: {time_taken:.4f}s / {current_time-self.init_time:.4f} s'
+                    last time consumption/overall running time: {time_taken:.2f}s / {current_time-self.init_time:.2f} s'
             )
             
             for k in self.keys:
@@ -271,7 +271,7 @@ class DummyLogger(Logger):
             f'Episode: {self.current_episode}/{self.args.max_episodes} ({100*self.current_episode/self.args.max_episodes:.4f}%), \
                 avg. reward: {np.mean(self.epi_rewards[-self.avg_window:]):.4f}, \
                 avg. length: {np.mean(self.epi_length[-self.avg_window:])},\
-                last time consumption/overall running time: {time_taken:.4f}s / {current_time-self.init_time:.4f} s'
+                last time consumption/overall running time: {time_taken:.2f}s / {current_time-self.init_time:.2f} s'
         )
 
 