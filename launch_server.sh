638f6d0f2599cb16818dc64bc76d4743392f91bc
quantumiracle <zhding@mail.ustc.edu.cn>
2022-04-04 23:25:03 -0400

    t

--- launch_server.sh
+++ launch_server.sh
@@ -9,13 +9,13 @@ declare -a envs=('slimevolley_SlimeVolley-v0' 'pettingzoo_boxing_v1')
 # declare -a envs=('pettingzoo_surround_v1' 'pettingzoo_tennis_v2')
 
 # declare -a methods=('selfplay2' 'fictitious_selfplay2' 'nxdo2' 'nfsp' 'nash_dqn' 'nash_dqn_exploiter')
-declare -a methods=('nash_dqn_factorized')
-# declare -a methods=('nash_dqn' 'nash_dqn_factorized')
+# declare -a methods=('nash_dqn_factorized')
+declare -a methods=('nash_dqn' 'nash_dqn_factorized')
 mkdir -p log/$DATE
 
 for i in ${!envs[@]}; do
     for j in ${!methods[@]}; do
-        echo CUDA_VISIBLE_DEVICES=$((i + 2)) python general_launch.py --env ${envs[$i]} --method ${methods[$j]} --save_id $DATE output log to: log/$DATE/${envs[$i]}_${methods[$j]}.log &
-        CUDA_VISIBLE_DEVICES=$((i + 2)) nohup python general_launch.py --env ${envs[$i]} --method ${methods[$j]} --save_id $DATE >> log/$DATE/${envs[$i]}_${methods[$j]}.log &
+        echo CUDA_VISIBLE_DEVICES=$((i + 0)) python general_launch.py --env ${envs[$i]} --method ${methods[$j]} --save_id $DATE output log to: log/$DATE/${envs[$i]}_${methods[$j]}.log &
+        CUDA_VISIBLE_DEVICES=$((i + 0)) nohup python general_launch.py --env ${envs[$i]} --method ${methods[$j]} --save_id $DATE >> log/$DATE/${envs[$i]}_${methods[$j]}.log &
     done
 done
