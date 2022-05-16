38681c63dcd7b784432b4a33e7af2670b4d89f05
quantumiracle <zhding@mail.ustc.edu.cn>
2022-05-16 13:20:05 -0400

    fix fsp

--- test_arbitrary_mdp.py
+++ test_arbitrary_mdp.py
@@ -14,7 +14,7 @@ parser_args = parser.parse_args()
 
 ### Load configurations
 
-method = ['selfplay2', 'fictitious_selfplay2', 'nash_dqn', 'nash_dqn_exploiter', 'nxdo2', 'nfsp', 'nash_ppo'][0]
+method = ['selfplay2', 'fictitious_selfplay2', 'nash_dqn', 'nash_dqn_exploiter', 'nxdo2', 'nfsp', 'nash_ppo'][1]
 prefix = 'mars/confs/mdp/mdp_arbitrary_mdp_'
 yaml_file = prefix + method
 
