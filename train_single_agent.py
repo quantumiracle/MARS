e44a8a0712ba08ddd9299fa88084b468e7187a7e
quantumiracle <zhding@mail.ustc.edu.cn>
2022-10-20 21:03:01 -0400

    fix

---
+++ train_single_agent.py
@@ -0,0 +1,26 @@
+from mars.utils.func import LoadYAML2Dict
+from mars.env.import_env import make_env
+from mars.rollout import rollout
+from mars.rl.agents import *
+from mars.rl.agents.multiagent import MultiAgent
+from mars.utils.args_parser import get_args
+from mars.utils.data_struct import AttrDict
+
+def launch():
+    args = get_args()
+    print('args: ', args)
+
+    ### Create env
+    env = make_env(args)
+    print(env)
+
+    ### Specify models for each agent     
+    model = eval(args.algorithm)(env, args)
+
+    model = MultiAgent(env, [model], args)
+
+    ### Rollout
+    rollout(env, model, args, args.save_id)
+
+if __name__ == '__main__':
+    launch()  # vars: Namespace -> dict
