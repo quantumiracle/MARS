3f3bdc47ad1617688d39b644343cb2191ddbf48a
quantumiracle <1402434478@qq.com>
2022-12-21 03:15:32 +0000

    fix code

--- test_single_agent_rl.py
+++ test_single_agent_rl.py
@@ -5,18 +5,18 @@ from mars.rollout import rollout
 from mars.rl.agents import *
 from mars.rl.agents.multiagent import MultiAgent
 
-
+env = ['pong_v5', 'boxing_v5'][1]
 # yaml_file = 'mars/confs/gym/cartpole_v1/gym_cartpole_v1_ppo' #PATH TO YAML
 # yaml_file = 'mars/confs/gym/mountaincarcontinuous_v0/gym_mountaincarcontinuous_v0_ppo' #PATH TO YAML
 # yaml_file = 'mars/confs/gym/ant_v2/gym_ant_v2_ppo' #PATH TO YAML
 # yaml_file = 'mars/confs/gym/hopper_v2/gym_hopper_v2_ppo' #PATH TO YAML
-yaml_file = 'mars/confs/gym/pong_v5/gym_pong_v5_dqn' #PATH TO YAML
+yaml_file = f'mars/confs/gym/{env}/gym_{env}_dqn' #PATH TO YAML
 
 
 args = LoadYAML2Dict(yaml_file, toAttr=True)
 args.record_video = True
 args.wandb_activate = True
-args.wandb_project = 'pong-v5'
+args.wandb_project = env
 args.wandb_entity = 'quantumiracle'
 init_wandb(args)
 ### Create env
